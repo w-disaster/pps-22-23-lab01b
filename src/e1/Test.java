9db1b1245051ae7e902ab395d0490a21b76af9c7
Gianluca Aguzzi <gianluca.aguzzi@studio.unibo.it>
2023-03-05 21:12:26 +0100

    lab-1b: minor in the description and project structure

--- src/e1/Test.java
+++ src/e1/Test.java
@@ -3,34 +3,35 @@ package e1;
 public class Test {
 
     /*
-     * Considerare il programmino di cui sotto, già funzionante: si provi ad usarlo. 
-     * E' realizzato con classe GUI, e interfaccia/implementazione per la logica. 
-     * Scopo del task è rifattorizzarlo migliorandone la qualità, e ottenendo un buon 
-     * testing delle sue varie parti (non la GUI, solo la Logica!).
+     * Considerare il programma di cui sotto, già funzionante e si provi ad usarlo.
+     * È realizzato con classe GUI e interfaccia/implementazione per la logica.
+     * Scopo dell'esercizio è rifattorizzare il la logica progetto (non la GUI)
+     * migliorandone la qualità, e ottenendo un buon testing delle sue varie parti.
+     *
      * Si propone il seguente approccio TDD.
      * 
      * Primo passo, sigilliamo LogicsImpl:
-     * 1) si scrivano alcuni test che passano su Logics/LogicsImpl, e che ci sembrano
-     * catturare il grosso del funzionamento della logica
+     * 1) si scrivano alcuni test che passano su Logics/LogicsImpl,
+     *  e che ci sembrano catturare il grosso del funzionamento della logica
      * 2) Se nel farlo servono piccole aggiunte a Logics/LogicsImpl le si implementino
      * 3) Non migliorare ora Logics/LogicsImpl, lo faremo dopo
      * 4) Si ri-verifichi il funzionamento del sistema
      * 
      * Ora, iterativamente:
      * 1) si consideri un aspetto dell'implementazione di LogicsImpl che può essere esternalizzato 
-     * da LogicsImpl (SRP), ossia inserito in una classe di cui LogicsImpl si comporrà
-     * 2) si costruisca l'interfaccia e la sua implementazione con TDD, ossia con vari step di 
-     * red-green-refactor sulla nuova classe
+     *  da LogicsImpl (SRP), ossia inserito in una classe di cui LogicsImpl si comporrà
+     * 2) si costruisca l'interfaccia e la sua implementazione con TDD,
+     *  ossia con vari step di red-green-refactor sulla nuova classe
      * 3) si tolga il codice da LogicsImpl e si vada ad utilizzare quello nella nuova classe
      * 4) si rilancino i test di LogicsImpl e si facciano anche test a mano sulla GUI
      * 
      * Si proceda così fino a che LogicsImpl non si è significativamente ridotta. A questo punto:
      * 1) si usi TDD per migliorare quel che resta di LogicsImpl
-     * 2) si crei un disegno UML del sistema prodotto
+     * 2) si crei un diagramma UML del sistema prodotto
      * 
-     * Ecco cosa fa il programma: 
+     * Descrizione del programma proposto:
      * 
-     * Scopo di questo esercizio è realizzare una GUI con l'aspetto mostrato nell'immagine fig.png, fornita, 
+     * Scopo di questo esercizio è realizzare una GUI con l'aspetto mostrato nell'immagine fig.png fornita,
      * che contiene una griglia quadrata di pulsanti (5x5), in cui sono presenti un pedone indicato con "*" 
      * e un cavallo indicato con "K", tutti e 2 in posizione iniziale random.
      * 
@@ -41,7 +42,6 @@ public class Test {
      * ad esempio potrebbe muoversi solo in 3 caselle, ossia (3,1) mangiando il pedone, (2,2) e (2,4). 
      * Clickando in una posizione valida il cavallo viene spostato, altrimenti non deve succedere nulla.
      * Quando il cavallo va sopra il pedone, si chiuda l'applicazione.
-     * 
      *
      */
 
